7feb529fc3194a77bc1d7d09434eefef72a41da1
timepp <tongjunhui@live.cn>
2010-01-01 02:25:54 +0800

    sync change

--- timepp_lib/test/main.cpp
+++ timepp_lib/test/main.cpp
@@ -4,20 +4,18 @@
 #include <tplib.h>
 #include <cmdlineparser.h>
 #include <wincon.h>
+#include <opblock.h>
 
-int wmain(int argc, wchar_t *argv[])
+void test_old()
 {
-	setlocale(LC_ALL, "");
-	tp::sc::log_default_console_config();
-
 	ON_LEAVE(tp::log(tp::a2w("测试自动释放宏，此日志应该在程序退出时输出\n")));
 	tp::cmdline_parser parser;
-	parser.parse(argc, argv);
+//	parser.parse(argc, argv);
 	tp::log(tp::a2w(tp::czA("命令行:%s", tp::w2a(GetCommandLineW()))));
-	for (int i = 0; i < argc; i++)
+//	for (int i = 0; i < argc; i++)
 	{
-		tp::log_indenter li(2);
-		tp::log(tp::cz(L"%02d %s", i, argv[i]));
+//		tp::log_indenter li(2);
+//		tp::log(tp::cz(L"%02d %s", i, argv[i]));
 	}
 
 	tp::log(L"check main");
@@ -26,6 +24,18 @@ int wmain(int argc, wchar_t *argv[])
 		unsigned char tmp[] = "ABCDE\xcc\xdd\xee\x00""acc";
 		tp::log(2, tp::cz(L"the content of array(%d bytes)\n%s", sizeof(tmp), &tp::hex_dump(tmp, sizeof(tmp))));
 	}
+}
+
+int wmain(int argc, wchar_t *argv[])
+{
+	setlocale(LC_ALL, "");
+	tp::sc::log_default_console_config();
+
+//	OPBLOCK(L"aaa");
+//	OPBLOCK(L"bbb");
+	SETOP(L"ccc");
+
+	tp::log(tp::cz(L"%s", CURRENT_OPLIST().c_str()));
 
 	return 0;
 }
